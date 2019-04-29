3b01b7cb3ccca483adb414f2cb92e0e68c38e2e3
00000sz <sz@mailsecured.com.au>
2019-04-29 20:01:23 +1000

    initial commit

---
+++ update.py
@@ -0,0 +1,38 @@
+import subprocess
+import requests
+from requests.exceptions import HTTPError
+
+feedurl = 'https://example.com/blacklist-domains.csv'
+bindconf = 'named.conf.blockeddomains'
+certbundle = 'CertBundle.pem'
+
+try:
+    print('Fetching new blacklist from '+feedurl)
+    r = requests.get(feedurl,verify=certbundle)
+    r.raise_for_status()
+except HTTPError as http_err:
+    print(http_err)
+except Exception as err:
+    print(err)
+else:
+    print('Success!')
+    print(r.status_code)
+    list = []
+    lines = r.text.split(',')
+    for l in lines:
+        try:
+            domain = str(l).strip()
+            print('Found domain: '+domain)
+            line = 'zone "'+domain+'" {type master; file "sinkhole.zone";};'
+            list.append(line)
+        except:
+            pass
+    print('Contructed list:')
+    print(list)
+    print('Opening BIND conf: '+bindconf)
+    with open(bindconf, 'w') as f:
+        for i in list:
+            f.write("%s\n" % i)
+    print('Wrote config.... Done!')
+    print('Reloading zones')
+    subprocess.call(['/usr/sbin/rndc','reload'])
